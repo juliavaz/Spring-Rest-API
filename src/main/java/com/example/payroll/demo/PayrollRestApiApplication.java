17506dbea3bab900f357e02bc732c202dee6fda8
Julia Vaz <juliavaz.dev@gmail.com>
2020-07-17 17:33:43 -0300

    remove package api and formatted PayrollRestApiApplication class

--- src/main/java/com/example/payroll/demo/PayrollRestApiApplication.java
+++ src/main/java/com/example/payroll/demo/PayrollRestApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PayrollRestApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PayrollRestApiApplication.class, args);
     }
-
 }
