8596b67cd72ecde7494c594ec36a91e26da04fe1
Julia Vaz <juliavaz.dev@gmail.com>
2020-07-17 16:31:17 -0300

    create package repository and EmployeeRepository class

---
+++ src/main/java/com/example/payroll/demo/Repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.payroll.demo.Repository;
+
+import com.example.payroll.demo.Model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
