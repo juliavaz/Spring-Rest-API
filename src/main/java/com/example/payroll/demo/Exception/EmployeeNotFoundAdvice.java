389adf353e623951f9972c42d5177598cb9a74ab
Julia Vaz <juliavaz.dev@gmail.com>
2020-07-17 20:33:49 -0300

    create EmployeeNotFoundAdvice class

---
+++ src/main/java/com/example/payroll/demo/Exception/EmployeeNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package com.example.payroll.demo.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EmployeeNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(EmployeeNotFoundException ex){
+        // classe de tratamento de resposta das exceptions
+        return ex.getMessage();
+    }
+}
