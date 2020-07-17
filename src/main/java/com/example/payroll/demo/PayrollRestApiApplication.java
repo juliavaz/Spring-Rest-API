d8ec62d7d53885f8a2676c750f8568de7056fb46
Julia Vaz <juliavaz.dev@gmail.com>
2020-07-17 15:57:11 -0300

    initial commit

---
+++ src/main/java/com/example/payroll/demo/PayrollRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.payroll.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollRestApiApplication.class, args);
+    }
+
+}
