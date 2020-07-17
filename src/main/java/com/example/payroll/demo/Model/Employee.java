7de852fd9d5f47ed80875f0dc845ab9a86af79d4
Julia Vaz <juliavaz.dev@gmail.com>
2020-07-17 16:21:23 -0300

    create package model and Employee class

---
+++ src/main/java/com/example/payroll/demo/Model/Employee.java
@@ -0,0 +1,72 @@
+package com.example.payroll.demo.Model;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    //
+    public Employee() {}
+
+    // Boa prática: construtor com no máximo 5/6 atributos
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) &&
+                Objects.equals(name, employee.name) &&
+                Objects.equals(role, employee.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
