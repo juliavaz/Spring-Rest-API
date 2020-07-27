a4174d501c4634a4154eb4f068809929c5220dbe
Julia Vaz <juliavaz.dev@gmail.com>
2020-07-27 00:05:21 -0300

    add ApiOperation and ApiResponses annotations

--- src/main/java/com/example/payroll/demo/Api/EmployeeController.java
+++ src/main/java/com/example/payroll/demo/Api/EmployeeController.java
@@ -3,10 +3,15 @@ package com.example.payroll.demo.Api;
 import com.example.payroll.demo.Exception.EmployeeNotFoundException;
 import com.example.payroll.demo.Model.Employee;
 import com.example.payroll.demo.Repository.EmployeeRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "Employees API REST")
 @RestController
 public class EmployeeController {
 
@@ -17,12 +22,28 @@ public class EmployeeController {
     }
 
     // recuperar informação da url
+    @ApiOperation(value = "Recupera informações da url",
+            response= Employee.class,
+            notes= "Recupera todas as informações da url")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Tudo certo", response = Employee.class),
+            @ApiResponse(code = 404, message = "Não encontrado", response = Employee.class)
+        }
+    )
     @GetMapping("/employees")
     List<Employee> all() {
         return repository.findAll();
     }
 
     // inserir um employee
+    @ApiOperation(value = "Insere um employee",
+            response= Employee.class,
+            notes= "Insere um novo employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Tudo certo", response = Employee.class),
+            @ApiResponse(code = 404, message = "Não encontrado", response = Employee.class)
+        }
+    )
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
@@ -31,12 +52,14 @@ public class EmployeeController {
     // itens individuais
 
     // recuperar informação de um employee específico, /employees/{tal}
+    @ApiOperation("Recupera informação de um employee especifíco")
     @GetMapping("/employees/{id}")
     Employee one(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
     }
 
     // alterar informações de um employee específico
+    @ApiOperation("Altera informações de um employee específico")
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
         // caso encontre o id pega o valor de name e role e atualiza salvando os dados do emplooye,
@@ -53,6 +76,7 @@ public class EmployeeController {
     }
 
     // método sem retorno que executa a ação de apagar o employee específico
+    @ApiOperation("Deleta um employee")
     @DeleteMapping("/employees/{id}")
     void deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
